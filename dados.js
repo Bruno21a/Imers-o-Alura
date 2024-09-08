1bd5ad020f166e5e354ac355bf47609b732acf03
Bruno21a <165836310+Bruno21a@users.noreply.github.com>
2024-09-07 22:37:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,65 @@
+let dados = [
+{
+    titulo : "Hulk",
+    descricao : "Hulk se tornou ídolo do Galo por sua capacidade de decidir jogos importantes, sua liderança dentro e fora de campo, sua humildade e identificação com a torcida, além de sua contribuição para a conquista de diversos títulos. Ele é um exemplo de jogador que se entrega de corpo e alma pelo clube e que conquistou o coração dos atleticanos.",
+    link : "https://www.transfermarkt.com.br/hulk/profil/spieler/80562"
+}
+,
+{
+    titulo : "Tardelli",
+    descricao : "Diego Tardelli se tornou um ídolo do Atlético Mineiro por sua capacidade de decidir jogos importantes, sua paixão pelo clube, sua identificação com a torcida e sua contribuição para a conquista de títulos históricos. Seu nome está para sempre ligado à história do Galo e ele será lembrado como um dos maiores jogadores a vestir a camisa alvinegra.",
+    link : "https://www.transfermarkt.com.br/diego-tardelli/profil/spieler/37536"
+},
+{
+    titulo : "Dadá",
+    descricao : "Dadá Maravilha é um ídolo do Atlético Mineiro por sua habilidade como artilheiro, por seus gols históricos, por sua identificação com a torcida, por seus títulos e por sua personalidade marcante. Sua história no Galo é um exemplo de dedicação, paixão e amor pelo clube.",
+    link : "https://www.transfermarkt.com.br/dada-maravilha/profil/spieler/290259"
+},
+{
+    titulo : "Reinaldo",
+    descricao : "José Reinaldo de Lima, mais conhecido como Reinaldo, é considerado um dos maiores gênios do futebol brasileiro em todos os tempos. Revelado pelas categorias de base do Clube, o ex-atacante vestiu a camisa alvinegra em 475 partidas e anotou 255 gols, que fazem dele o maior artilheiro da história do Galo.",
+    link : "https://www.transfermarkt.com.br/reinaldo/profil/spieler/135001"
+},
+{
+    titulo : "Ronaldinho",
+    descricao : "Anunciada no dia 4 de junho de 2012, a contratação de Ronaldinho Gaúcho gerou grande repercussão internacional. Eleito por duas vezes pela FIFA o melhor jogador de futebol do mundo, o craque encantou a Massa, conquistou os corações atleticanos e foi decisivo na conquista da Libertadores da América 2013, com passes milimétricos, gols importantes e espírito vencedor.",
+    link : "https://www.transfermarkt.com.br/ronaldinho/profil/spieler/3373"
+},
+{
+    titulo : "Marques",
+    descricao : "O atacante Marques teve três passagens pelo Galo: entre 1997 e 2002, entre 2005 e 2006 e entre 2008 e 2010. Dono de dribles curtos e agilidade, deixava os companheiros de ataque na cara do gol. Em 1999, fez a célebre dupla ofensiva do time vice-campeão brasileiro com Guilherme. Marques é o jogador que mais vestiu a camisa do Atlético na história do Campeonato Brasileiro, tendo atuado 192 vezes pela competição. Disputou, ao todo, 386 jogos pelo Galo, marcou 133 gols e conquistou cinco títulos.",
+    link : "https://www.transfermarkt.com.br/marques/profil/spieler/67796"
+},
+{
+    titulo : "Pierre",
+    descricao : "Contratado em 2011 para jogar no Galo, o volante Pierre não precisou de muito tempo para conquistar a torcida Atleticana, que encantou-se pela garra e vontade do jogador em campo e o batizou de pitbull. Pierre foi o jogador que mais entrou em campo com a camisa do Galo em 2012. No ano seguinte, 2013, participou de todos os jogos da campanha do título da Libertadores. Entre 2011 e 2015, atuou em 170 jogos e conquistou quatro títulos pelo Atlético. Na saída, publicou uma carta de despedida enaltecendo a Massa, a que carinhosamente chamou de “torcida do Eu Acredito”.",
+    link : "https://www.transfermarkt.com.br/pierre/profil/spieler/53275"
+},
+{
+    titulo : "Renaldo",
+    descricao : "O atacante Renaldo teve duas passagens pelo Galo: entre 1993 e 1996 e em 2002. Goleador nato, foi o artilheiro do Campeonato Mineiro de 1995 com 13 gols e do Campeonato Brasileiro de 1996 com 16 gols. Ao todo, jogou 183 vezes com a camisa do Clube, marcou 79 gols e conquistou um título.",
+    link : "https://www.transfermarkt.com.br/renaldo/profil/spieler/253469"
+},
+{
+    titulo : "Luan",
+    descricao : "O meio-campista Luan conquistou a torcida Atleticana com seu estilo de jogo. Atuou no Galo entre 2013 e 2019 e sempre será lembrado pelas atuações na campanha do título da Copa do Brasil de 2014. Luan marcou gols nas viradas históricas do Atlético no Mineirão contra o Corinthians, pelas quartas-de-final, e o Flamengo, pelas semifinais da competição. Também deixou sua marca ao fazer o primeiro gol da final contra o Cruzeiro. Ao todo, Luan jogou 305 vezes com a camisa do Galo, marcou 49 gols e conquistou sete títulos.",
+    link : "https://www.transfermarkt.com.br/luan-madson/profil/spieler/192318"
+},
+{
+    titulo : "Donizete",
+    descricao : "Jogador de qualidade na saída de bola e raça nas jogadas defensivas, o volante Leandro Donizete jogou no Galo entre 2012 e 2016. Participou das campanhas dos títulos da Copa Libertadores de 2013 e da Copa do Brasil de 2014. Jogou 231 vezes com a camisa do Clube, marcou quatro gols e conquistou sete títulos.",
+    link : "https://www.transfermarkt.com.br/leandro-donizete/profil/spieler/68784"
+},
+{
+    titulo : "Guilherme",
+    descricao : "Guilherme foi um dos principais goleadores da história do Galo. Com a camisa alvinegra, foi o artilheiro do Brasileirão de 1999, com 28 gols. Naquele ano, Guilherme formou dupla de ataque memorável com Marques e o Atlético foi vice-campeão brasileiro. Em duas passagens pelo Clube, o atacante, que nasceu em Marília-SP, vestiu a camisa do Galo em 205 partidas e marcou 139 gols.",
+    link : "https://www.transfermarkt.com.br/guilherme-alves/profil/spieler/20408"
+},
+{
+    titulo : "Dátolo",
+    descricao : "O meia argentino Dátolo jogou no Galo entre 2013 e 2016. Vestiu a camisa Alvinegra em 127 partidas, marcou 18 gols e conquistou quatro títulos. O jogador apresentava uma das características mais valorizadas pela Massa Atleticana, a raça, e é lembrado por ser autor de um dos gols do time na final da Copa do Brasil de 2014, a primeira em que o Galo sagrou-se campeão da competição.",
+    link : "https://www.transfermarkt.com.br/jesus-datolo/profil/spieler/30759"
+}
+];
+
+
